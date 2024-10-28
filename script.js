32dca1851e96d309f49bd72a38dadd7f1856e70b
Plackett <johnnicholasdimitri@gmail.com>
2024-10-27 23:32:20 -0400

    svelte init

--- script.js
+++
@@ -1,8 +0,0 @@
-function shiftPage(page){
-  document.getElementById(page).display = "block";
-  document.getElementById(page + "b").classList.add("select");
-  if(page != "home"){
-    document.getElementById("home").display = "none";
-    document.getElementById("home").classList.remove("select");
-  }
-}
