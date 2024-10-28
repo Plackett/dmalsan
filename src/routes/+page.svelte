0720a23156a43536ab59b897be1b648fffd0b3b1
Niko Krinos <johnnicholasdimitri@gmail.com>
2024-10-28 18:46:37 -0400

    add info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Niko Krinos portfolio website" />
 </svelte:head>
 
 <script>
@@ -40,7 +40,7 @@
 		alt="This is a picture of my face"
 		style={imageStyle}
 	/>
-	<div class="mr-20" style={divStyle}>
+	<div class="mr-20 " style={divStyle}>
 		<h1 class="text-2xl">
 			John-Nicholas (Niko) Krinos
 		</h1>
@@ -52,6 +52,26 @@
 		</a>
 		<p class="pr-10">This website will serve as my portfolio and will be updated as I work on more projects!</p>
 		<h1 class="text-2xl pt-20">Latest Projects:</h1>
+		
+		<h2 class="text-xl pt-5">Lyrics2go</h2>
+		<p><strong>Code Education - Project Manager</strong></p>
+		<p>Sep 2024 - Nov 2024</p>
+		<p>We created a mobile app for iOS and Android using React Native on the Expo Framework that can display the lyrics to any song recognized by the MusicBrainz API. This project lasted for four sprints, with the last sprint dedicated to producing a presentation describing the app.</p>
+		<p><strong>Skills:</strong> Git, GitHub, React Native, Mobile Application Development</p>
+		<p><a href="https://github.com/Plackett/Lyrics2go-fall2024" target="_blank" rel="noopener noreferrer">View the Lyrics2go repository on GitHub</a></p>
+
+		<h2 class="text-xl pt-5">Six Degrees</h2>
+		<p><strong>Front-End Developer</strong></p>
+		<p>Aug 2020 - Jun 2021</p>
+		<p>With a team of two other students, we created a mobile app for iOS and Android using Dart on the Flutter framework. We placed 10th nationally at the Future Business Leaders of America National Leadership Conference for the 2020-2021 school year.</p>
+		<p><strong>Skills:</strong> Dart, Flutter, Mobile Application Development</p>
+
+		<h2 class="text-xl pt-5">Wizterria</h2>
+		<p><strong>Programming Director</strong></p>
+		<p>Developed as a video game using C++ with SDL 3.0, Wizterria offers an engaging interactive experience.</p>
+		<p><strong>Skills:</strong> C++, SDL 3.0, Game Development, Team Collaboration</p>
+		<p><a href="https://sites.google.com/view/wizterria/home" target="_blank" rel="noopener noreferrer">Visit the Wizterria project site</a></p>
+
 	</div>
 </section>
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,23 +4,36 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
-
+	<h1 class="text-2xl">
+		About Me
+	</h1>
 	<p>
-		This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
-		the following into your command line and following the prompts:
+		I was born in Tampa, Florida.
 	</p>
+	<h1 class="text-2xl pt-20">Education:</h1>
+	<p>Florida State University, Tallahassee, FL (Expected 2028)</p>
+	<p>Bachelor of Science in Computer Science</p>
+	<p>Middleton High School, Tampa, FL (May 2024)</p>
+	<p>3.98 Cumulative State GPA; 6.44 Cumulative District GPA</p>
 
-	<pre>npx sv create</pre>
+	<h1 class="text-2xl pt-20">Certifications:</h1>
+	<ul>
+		<li>Microsoft Office Specialist: Excel, Word, PowerPoint</li>
+		<li>Adobe Certified Professional: Graphic Design, Photoshop, InDesign, Animate, Dreamweaver</li>
+		<li>Microsoft Technology Associate: Security, Networking, Mobility, Programming with JavaScript, HTML, Windows OS Fundamentals</li>
+		<li>Autodesk Certified User: 3ds Max, Inventor</li>
+		<li>Unity Certified User: VR Developer, Artist, Programmer</li>
+		<li>PMI Project Management Ready</li>
+		<li>CompTIA IT Fundamentals (ITF+)</li>
+	</ul>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+	<h1 class="text-2xl pt-20">Volunteer Experience:</h1>
+	<p><strong>Feeding Tampa Bay:</strong> Contributed over 30 hours in food donation events.</p>
+	<p><strong>Tampa Greek Festival:</strong> Served over 30 hours managing transactions and assisting with operations.</p>
+	<p><strong>Tampa Coastal Cleanup:</strong> Participated in beach clean-ups, gaining teamwork experience.</p>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+	<h1 class="text-2xl pt-20">Skills:</h1>
+	<p>Languages: Intermediate American Sign Language, Elementary French</p>
+	<p>Technical: Project Management Ready, Adobe Suite, Microsoft Office</p>
+	<p>Programming: C, C++, Java, JavaScript, Python, Actionscript 3.0, Flutter, Dart, React, Expo</p>
 </div>
