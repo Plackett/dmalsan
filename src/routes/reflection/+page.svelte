5b73c9a0ea67aaf318153da60ba7ba161594b7d2
Plackett <johnnicholasdimitri@gmail.com>
2024-12-06 20:14:25 -0500

    final commit

--- src/routes/reflection/+page.svelte
+++ src/routes/reflection/+page.svelte
@@ -7,23 +7,42 @@
         
         <!-- Reflection Content -->
         <p class="text-lg leading-8 text-gray-700 dark:text-gray-300 font-serif">
-			The process of creating this e-Portfolio has been beneficial to my growth as an author, web-designer, and student.
-			This is evident in every page of this portfolio, from the home page which I created first, to this reflection page which was the last one I made, each page's development history demonstrates my progress over time.
-			Although I have gone over and revised every page in this website, I did not change the underlying structure, keeping it the same way that I started it.
-			Additionally, over the development of this page and this very reflection I uploaded my everyday progress on the Source area linked in the footer on every page of the site.
-        </p>
+			The process of creating this e-portfolio has been beneficial to my growth as an author, web designer, and student. 
+			This is evident in every page of this portfolio, from the home page I created first, to this reflection page which was the last one I made, each page's development history demonstrates my progress over time. 
+			Although I have gone over and revised every page on this website, I did not change the underlying structure, keeping it the same way that I started it. 
+			I believe this choice shows how initial ideas stay relevant even with revision and better execution.
+		</p>
+
+		<p class="mt-4 text-lg leading-8 text-gray-700 dark:text-gray-300 font-serif">
+			Additionally, over the development of this page and this very reflection, I uploaded my everyday progress on the Source area linked in the footer on every page of the site. 
+			Working on the project this way allowed me to add notes and documentation for every significant change I made and find an old revision of the website if I didn't like a newer revision. 
+			This archive of my work functions as transparency to the users of the website as well since they can see the code behind the site and take it for themselves to modify if they desire.
+		</p>
 
         <p class="mt-4 text-lg leading-8 text-gray-700 dark:text-gray-300 font-serif">
-            There were some initial hurdles when making the website, especially with the home page because I kept having issues with navigation and the image animations.
-			Thankfully all of the issues were rectified with time and I was able to fix them with ease.
-			Following these homepage issues I started creating pages based on what I saw on previous classes e-Portfolios but quickly realized that some pages were unnecessary.
-			A good example of this is the About Me page which I originally included and is on most e-Portfolios.
-            I decided to remove this page because I realized with time that this entire website is an About Me page because it shows the reader what I can make and my thought process.
+            There were some initial hurdles when making the website, especially with the home page because I kept having issues with navigation and the image animations. 
+			Thankfully all of the problems were rectified with time and I was able to fix them with ease. 
+			Following these homepage issues, I started creating pages based on what I saw on previous classes' e-Portfolios but quickly realized that some pages were unnecessary. 
+			A good example of this is the About Me page which I originally included and is on most e-Portfolios. 
+			I decided to remove this page because I realized with time that this entire website is an About Me page. 
+			After all, it shows the reader what I can make and my thought process. 
 			While I agree that it is easier to be served a well-written page in paragraph form detailing an entire person's life, listing projects with descriptions can be much more informative.
         </p>
 
-        <p class="mt-4 text-lg leading-8 text-gray-700 dark:text-gray-300 font-serif">
-            Next, 
+		<p class="mt-4 text-lg leading-8 text-gray-700 dark:text-gray-300 font-serif">
+            Next, we were tasked with revising one of the two formal essays we wrote during the semester. 
+			I chose project two for this because it was much easier for me to write initially and was more enjoyable because of its predefined structure. 
+			For my revisions, I decided to modify the thesis statement to develop an argument on the message of the artifacts and modify my evidence statements accordingly. 
+			I like to finish my revisions when I submit an assignment and not look back on the past, but this project opened my eyes to the possibilities of changing the existing writing to better fit a new direction I've taken in my research.
         </p>
+
+        <p class="mt-4 text-lg leading-8 text-gray-700 dark:text-gray-300 font-serif">
+            All in all, I enjoyed making this project and I can't help but recommend it for this class in the future. 
+			It was a refreshing break from the stress of formal papers and gave me the motivation to put some good effort in before exam week. 
+			Despite the challenges I encountered while creating it, from the homepage not working to the navigation failing, having to select two artifacts from my entire semester of class, and writing this reflection on it all, I still think this was a positive experience!
+        </p>
+		<p class="mt-4 text-lg text-right leading-8 text-gray-700 dark:text-gray-300 font-serif">
+			- John-Nicholas (Niko) Krinos
+		</p>
     </div>
 </section>
